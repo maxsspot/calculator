64486f8aeb28103c0bc8ccc15421c554e7e3aeb6
nope <55818423+maxsspot@users.noreply.github.com>
2025-10-26 17:37:51 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+function buttonsWork() {
+    const buttons = document.getElementsByTagName("button");
+    const calculations = document.getElementById("calculations");
+    const showExtraKeys = document.getElementById("extraKeysControls");
+    let isOpen = false;
+
+    for (i=0;i<buttons.length;i++) {
+        buttons[i].onclick = function () {
+            if (this.textContent == "DEL") {
+                calculations.value = calculations.value.slice(0,-1);
+            } else if (this.textContent == "C") {
+                calculations.value="";
+            } else if(this.textContent != "=") {
+                calculations.value+=this.textContent;
+            } else {
+                try {
+                    calculations.value=math.evaluate(calculations.value);
+                } catch (error) {
+                    calculations.value="Error In Calculation";
+                }
+            }
+        }
+    }
+
+    showExtraKeys.onclick = function () {
+        const extraKeys = document.getElementById("extraKeys");
+
+        if (!isOpen) {
+            extraKeys.style.display="inherit";
+            isOpen=true;
+            showExtraKeys.textContent="Hide Extra Keys ▲";
+            calculations.style.marginTop="9.375px";
+        } else {
+            extraKeys.style.display="none";
+            isOpen=false;
+            showExtraKeys.textContent="Show Extra Keys ▼";
+            calculations.style.marginTop="-5px";
+        }
+    }
+}
