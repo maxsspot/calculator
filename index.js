f79d1cba7f528d08c15665b86a6fdda4fd688770
nope <55818423+maxsspot@users.noreply.github.com>
2025-10-30 13:21:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,10 +41,8 @@ function buttonsWork() {
     }
     
     for(i=0;i<buttons.length;i++) {
-        if(buttons[i].length == 1) {
-            console.log(buttons[i]);
-            possibleKeys.push(buttons[i]);
-        }
+        console.log(buttons[i]);
+        possibleKeys.push(buttons[i]);
     }
 
     console.log(possibleKeys)
